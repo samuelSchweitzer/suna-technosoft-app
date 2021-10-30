adf07dc54a7ad640a161c0e684032dbf4c008036
samuel8055 <samueltschweitzer@gmail.com>
2021-10-30 22:38:03 +0530

    Worked on Contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,15 +1,44 @@
 import React from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
+import { MdLocationOn, MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
 import "./styles/Contact.scss";
 
 const Contact = () => {
   return (
     <div className="contact-section-wrapper">
       <div className="contact-section-card">
-        <div className="contact-card-left"></div>
+        <div className="contact-card-left-wrapper">
+          <div className="contact-card-left">
+            <h3>Get in touch</h3>
+
+            <div className="contact-icon-wrapper">
+              <div className="icon-wrapper">
+                <MdLocationOn className="location" />
+              </div>
+              <div className="icon-title">
+                7 Division St, New York, NY 10002, USA
+              </div>
+            </div>
+
+            <div className="contact-icon-wrapper">
+              <div className="icon-wrapper">
+                <FaPhoneAlt className="phone" />
+              </div>
+              <span>+91 994455 8822</span>
+            </div>
+
+            <div className="contact-icon-wrapper">
+              <div className="icon-wrapper">
+                <MdEmail className="email" />
+              </div>
+              <span>email@sunatechnosoft.com</span>
+            </div>
+          </div>
+        </div>
 
         <div className="contact-card-right">
-          <h3>Get in touch</h3>
+          <h3>Drop us a message</h3>
           <Form>
             <Row className="mb-3">
               <Form.Group as={Col} controlId="formGridEmail">
